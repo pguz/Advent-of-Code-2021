b82f0c3d098da1533fdae997e96bdae3315bdeb7
Paweł Guz <pawelguz@gmail.com>
2022-01-02 12:28:25 +0100

    Day 12

--- run_solution.py
+++ run_solution.py
@@ -5,7 +5,7 @@ import os.path
 import time
 
 from solutions import (day_02, day_03, day_04, day_05, day_06, day_07, day_08,
-                       day_09, day_10, day_11)
+                       day_09, day_10, day_11, day_12)
 
 day_to_solution_file_mapping = {
     "02": day_02,
@@ -18,6 +18,7 @@ day_to_solution_file_mapping = {
     "09": day_09,
     "10": day_10,
     "11": day_11,
+    "12": day_12,
 }
 
 
---
+++ solutions/day_12.py
@@ -0,0 +1,58 @@
+# Day 12: Passage Pathing
+from collections import defaultdict
+
+
+def parse_file(fd):
+    return ([tuple(line.split('-')) for line in fd.read().splitlines()],)
+
+
+def find_paths_small_once(caves_connections):
+    edges = defaultdict(list)
+
+    for b, e in caves_connections:
+        if e != 'start' and b != 'end':
+            edges[b].append(e)
+        if b != 'start' and e != 'end':
+            edges[e].append(b)
+
+    def _dfs(cave, small_caves_visited):
+        if cave == 'end':
+            return 1
+        if cave != 'start' and cave.islower():
+            if cave in small_caves_visited:
+                return 0
+            small_caves_visited.add(cave)
+
+        return sum([_dfs(v, small_caves_visited.copy()) for v in edges[cave]])
+    
+    return _dfs('start', small_caves_visited=set())
+
+
+def find_paths_one_small_twice(caves_connections):
+    edges = defaultdict(list)
+
+    for b, e in caves_connections:
+        if e != 'start' and b != 'end':
+            edges[b].append(e)
+        if b != 'start' and e != 'end':
+            edges[e].append(b)
+            
+    def _dfs(cave, small_caves_visited, small_cave_visited_twice):
+        if cave == 'end':
+            return 1
+        if cave != 'start' and cave.islower():
+            if cave in small_caves_visited:
+                if small_cave_visited_twice is False:
+                    small_cave_visited_twice = True
+                else:
+                    return 0
+            else:
+                small_caves_visited.add(cave)
+        
+        return sum([_dfs(v, small_caves_visited.copy(), small_cave_visited_twice) for v in edges[cave]])
+
+    return _dfs('start', small_caves_visited=set(), small_cave_visited_twice=False)
+
+
+solution_function_01 = find_paths_small_once
+solution_function_02 = find_paths_one_small_twice
